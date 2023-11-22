bf2dfedd2189d7b260e1a40c93904772f2d6d8bc
huahuahuahuahuahuahuahua <iceice2223@outlook.com>
2023-11-22 12:04:17 +0800

    Optimize performance

--- src/nf_nat_fullcone.h
+++ src/nf_nat_fullcone.h
@@ -27,6 +27,8 @@
 #include <net/netfilter/nf_conntrack_core.h>
 #include <net/netfilter/nf_conntrack_ecache.h>
 
+#define HASHTABLE_BUCKET_BITS 14
+
 #ifndef NF_NAT_RANGE_PROTO_RANDOM_FULLY
 #define NF_NAT_RANGE_PROTO_RANDOM_FULLY (1 << 4)
 #endif
@@ -50,87 +52,51 @@ static inline void nf_ct_netns_put(struct net *net, u8 nfproto)
  * @NFTA_FULLCONE_FLAGS: NAT flags (see NF_NAT_RANGE_* in linux/netfilter/nf_nat.h) (NLA_U32)
  */
 enum nft_fullcone_attributes {
-	NFTA_FULLCONE_UNSPEC,
-	NFTA_FULLCONE_REG_PROTO_MIN,
-	NFTA_FULLCONE_REG_PROTO_MAX,
-	NFTA_FULLCONE_FLAGS,
-	__NFTA_FULLCONE_MAX
+    NFTA_FULLCONE_UNSPEC,
+    NFTA_FULLCONE_REG_PROTO_MIN,
+    NFTA_FULLCONE_REG_PROTO_MAX,
+    NFTA_FULLCONE_FLAGS,
+    __NFTA_FULLCONE_MAX
 };
+
 #define NFTA_FULLCONE_MAX		(__NFTA_FULLCONE_MAX - 1)
 
 /* fullcone specific data structures */
 
 struct nat_mapping_original_tuple {
-	struct nf_conntrack_tuple tuple;
+    struct nf_conntrack_tuple tuple;
 
-	struct list_head node;
+    struct list_head node;
 };
 
 struct nat_mapping {
-	uint16_t port;		/* external source port */
-	__be32 addr;		/* external source ip address */
+    uint16_t port; /* external source port */
+    __be32 addr; /* external source ip address */
 
-	__be32 int_addr;	/* internal source ip address */
-	uint16_t int_port;	/* internal source port */
+    __be32 int_addr; /* internal source ip address */
+    uint16_t int_port; /* internal source port */
 
-	int refer_count;	/* how many references linked to this mapping
-				 * aka. length of original_tuple_list */
-
-	struct list_head original_tuple_list;
-
-	struct hlist_node node_by_ext_port;
-	struct hlist_node node_by_int_src;
-
+    struct hlist_node node_by_ext_port;
+    struct hlist_node node_by_int_src;
 };
-
-#if IS_ENABLED(CONFIG_NF_NAT_IPV6) || (IS_ENABLED(CONFIG_IPV6) && LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0))
-struct nat_mapping6 {
-	uint16_t port;		/* external source port */
-	union nf_inet_addr addr;	/* external source ip address */
-
-	union nf_inet_addr int_addr;	/* internal source ip address */
-	uint16_t int_port;	/* internal source port */
-
-	int refer_count;	/* how many references linked to this mapping
-				 * aka. length of original_tuple_list */
-
-	struct list_head original_tuple_list;
-
-	struct hlist_node node_by_ext_port;
-	struct hlist_node node_by_int_src;
-
-};
-#endif
 
 struct tuple_list {
-	struct nf_conntrack_tuple tuple_original;
-	struct nf_conntrack_tuple tuple_reply;
-	struct list_head list;
+    struct nf_conntrack_tuple tuple_original;
+    struct nf_conntrack_tuple tuple_reply;
+    struct list_head list;
 };
 
 /* fullcone specific data structures end */
 
 // NOTE: declaration listed here must use EXPORT_SYMBOL_*
 
-unsigned int nf_nat_fullcone_ipv4(struct sk_buff *skb, unsigned int hooknum,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
-				  struct nf_nat_range2 *range,
-#else
-				  struct nf_nat_range *range,
-#endif
-				  const struct net_device *out);
+unsigned int nf_nat_fullcone_ipv4(struct sk_buff* skb, unsigned int hooknum,
+                                  struct nf_nat_range2* range,
+                                  const struct net_device* out);
 
-unsigned int nf_nat_fullcone_ipv6(struct sk_buff *skb, unsigned int hooknum,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
-				  struct nf_nat_range2 *range,
-#else
-				  struct nf_nat_range *range,
-#endif
-				  const struct net_device *out);
-
-void nf_nat_fullcone_handle_dying_tuples(void);
 void nf_nat_fullcone_destroy_mappings(void);
-void nf_nat_fullcone_dying_tuple_list_add(struct list_head *new_dying);
+
+void nf_nat_fullcone_dying_tuple_list_add(struct list_head* new_dying);
 
 /*
  * For [FUTURE] usage
